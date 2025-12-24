5d249c2861aa9166e365902d3f677926b03e9212
Alit Wira <alitwira08@gmail.com>
2025-12-24 21:45:18 +0800

    done

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Home, ShoppingBag, User } from 'lucide-react'
+import { Home, ShoppingBag, User, Package } from 'lucide-react'
 import Link from 'next/link'
 
 export default function Nav() {
@@ -10,7 +10,10 @@ export default function Nav() {
           <Home size={20} className='text-gray-600' />
           <span className='text-xs font-light text-gray-600'>Beranda</span>
         </Link>
-    
+        <Link href="/Product" className='flex-1 flex flex-col items-center gap-1 p-2 rounded-lg hover:bg-gray-50 transition'>
+          <Package size={20} className='text-gray-600' />
+          <span className='text-xs font-light text-gray-600'>Produk</span>
+        </Link>
         <Link href="/Pesanan" className='flex-1 flex flex-col items-center gap-1 p-2 rounded-lg hover:bg-gray-50 transition'>
           <ShoppingBag size={20} className='text-gray-600' />
           <span className='text-xs font-light text-gray-600'>Pesanan</span>
